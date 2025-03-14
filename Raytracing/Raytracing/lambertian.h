0fb51eb3f8f2fa43bd2f1f06a58de7d4ba384142
Seth Duncan <nomadtadpole@gmail.com>
2025-03-14 11:43:41 +0100

    update metal

--- Raytracing/Raytracing/lambertian.h
+++ Raytracing/Raytracing/lambertian.h
@@ -1,14 +1,17 @@
 #pragma once
 #include "material.h"
 
+
+
 class lambertian : public material {
 public:
 	lambertian(const vec3 a) : albedo(a) {}
 	virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered, std::mt19937_64& rng, std::uniform_real_distribution<double>& unif) const override {
-		vec3 target = rec.p + rec.normal + random_in_unit_sphere(rng,unif);
+		vec3 target = rec.p + rec.normal + randomInUnitSphere(rng,unif);
 		scattered = ray(rec.p, target - rec.p);
 		attenuation = albedo;
 		return true;
 	}
 	vec3 albedo;
 };
+
