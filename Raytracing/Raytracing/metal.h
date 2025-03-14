999af63bd576c2e6c34122a026f05c397778db0e
Seth Duncan <nomadtadpole@gmail.com>
2025-03-14 12:02:14 +0100

    Debut des dielectric

--- Raytracing/Raytracing/metal.h
+++ Raytracing/Raytracing/metal.h
@@ -3,14 +3,15 @@
 
 class metal : public material {
 public:
-	metal(const vec3& a) : albedo(a) {}
+	metal(const vec3& a, float f) : albedo(a) { if (f < 1) fuzz = f; else fuzz = 1; }
 	virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered,std::mt19937_64& rng, std::uniform_real_distribution<double>& unif) const override {
 		vec3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
-		scattered = ray(rec.p, reflected);
+		scattered = ray(rec.p, reflected + fuzz*randomInUnitSphere(rng,unif));
 		attenuation = albedo;
 		return (dot(scattered.direction(), rec.normal) > 0);
 	}
 	vec3 albedo;
+	float fuzz;
 private:
 	vec3 reflect(const vec3& v, const vec3& n)  const {
 		return v - 2 * dot(v, n) * n;
